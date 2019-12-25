58944686d652fcc341b9ac5a3e6537faa7b8a6eb
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2019-12-25 21:22:34 +0100

    code refactor

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// DOM selection
+const url = 'https://randomuser.me/api';
+const btn = document.querySelector('button');
+const avatar = document.querySelector('#avatar');
+const fullname = document.querySelector('#fullname');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const city = document.querySelector('#city');
+const nationalty = document.querySelector('#nationalty');
+
+
+
+const handleErrors = res => {
+    if (!res.ok) {
+        throw Error(res.status);
+    }
+
+    return res.json();
+};
+
+const updateProfile = data => {
+    console.log(data);
+    let userAvatar = (data.results[0].picture.medium);
+    let name = (
+        `${data.results[0].name.first} ${data.results[0].name.last}`
+    );
+    let loginUsername = (data.results[0].login.username);
+    let userEmail = (data.results[0].email);
+    let userCity = (data.results[0].location.city);
+    let userNationalty = (data.results[0].nat);
+
+
+    avatar.src = userAvatar;
+    fullname.innerHTML = name;
+    username.textContent = loginUsername;
+    email.textContent = userEmail;
+    city.textContent = userCity;
+    nationalty.textContent = userNationalty;
+};
+
+btn.addEventListener('click', function () {
+    fetch(url)
+        .then(handleErrors)
+        .then(updateProfile)
+        .catch(err => {
+            console.log(`You have a ${err} error`);
+
+        });
+});
+
+
+
+        // fetch(url)
+        // .then(handleErrors)
+        // .then(parseJSON)
+        // .then(updateProfile)
+        // .catch(printError)
