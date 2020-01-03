4abbf1ec3822fdbe1b6da3612fd80f45352d5371
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-03 13:09:45 +0100

    cache files on install\

--- sw.js
+++ sw.js
@@ -1,12 +1,26 @@
-const cacheName = 'RUG-cache-v1';
+const cacheName = 'cache-v1';
 
 const cachedFiles = [
     '/',
-    'index.html',
-    'styles/style.css',
-    'assets/favicon/android-chrome-192x192.png',
+    '/index.html',
+    '/css/style.css',
+    '/js/script.js',
+    '/assets/favicon/android-chrome-192x192.png',
     'assets/favicon/android-chrome-512x512.png',
     'assets/favicon/apple-touch-icon.png',
     'assets/favicon/favicon-16x16.png',
     'manifest.json'
 ];
+
+self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(cachedFiles);
+            })
+    );
+});
+
