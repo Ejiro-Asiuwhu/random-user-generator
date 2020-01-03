ceb849b86f4d441bbbbfbb974caca28b719287d9
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-03 12:36:03 +0100

    add favicon

---
+++ sw.js
@@ -0,0 +1,12 @@
+const cacheName = 'RUG-cache-v1';
+
+const cachedFiles = [
+    '/',
+    'index.html',
+    'styles/style.css',
+    'assets/favicon/android-chrome-192x192.png',
+    'assets/favicon/android-chrome-512x512.png',
+    'assets/favicon/apple-touch-icon.png',
+    'assets/favicon/favicon-16x16.png',
+    'manifest.json'
+];
